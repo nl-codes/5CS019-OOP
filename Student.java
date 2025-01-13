dd0b6034a52c6230a1f1d1703111da2e60040411
narayanLohani <narayanlohani07@gmail.com>
2025-01-13 08:29:14 +0545

    Readd files to new repo

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public String name;
+    public int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
